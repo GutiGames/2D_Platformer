7ee3633abe88e917d244c68b01296b4188bc5cbf
JonanGuti <jonandergutierrez@gmail.com>
2015-10-02 20:38:05 +0200

    Menu (just took a random bg from the explorer)

--- src/main/java/jonanguti/platformer/GamePanel.java
+++ src/main/java/jonanguti/platformer/GamePanel.java
@@ -52,7 +52,7 @@ public class GamePanel extends JPanel implements Runnable,KeyListener {
 
     private void init(){
         image = new BufferedImage(WIDTH, HEIGHT, BufferedImage.TYPE_INT_RGB);
-        // maybe g = (Graphics2D) g;
+        g = (Graphics2D) image.getGraphics(); // LOL
 
         running = true;
 
@@ -76,6 +76,10 @@ public class GamePanel extends JPanel implements Runnable,KeyListener {
             elapsed = System.nanoTime() - start;
             wait = targetTime - elapsed / 1000000;
 
+            if (wait < 0){
+                wait = 5;
+            }
+
             try {
                 Thread.sleep(wait);
             }
@@ -95,7 +99,7 @@ public class GamePanel extends JPanel implements Runnable,KeyListener {
 
     public void drawToScreen(){
         Graphics g2 = getGraphics();
-        g2.drawImage(image, 0, 0, null);
+        g2.drawImage(image, 0, 0, WIDTH * SCALE, HEIGHT * SCALE, null);
         g2.dispose();
     }
 
--- src/main/java/jonanguti/platformer/GameStates/MenuState.java
+++ src/main/java/jonanguti/platformer/GameStates/MenuState.java
@@ -2,21 +2,112 @@ package main.java.jonanguti.platformer.GameStates;
 
 
 
+import main.java.jonanguti.platformer.Map.Background;
+
 import java.awt.*;
+import java.awt.event.KeyEvent;
 
 public class MenuState extends GameState {
 
-    // TODO private Background bg;
+    private Background bg;
+
+    private int currentChoice = 0;
+    private String[] options ={"Start","Help", "Quit"};
+
+    private Color titleColor;
+    private Font titleFont;
+
+    private Font font;
 
     public MenuState(GSManager gsm){
         this.gsm = gsm;
 
+        try{
+            bg = new Background("/Backgrounds/menubg.gif", 1);
+            bg.setVector(-0.1, 0);
+
+            titleColor = new Color(120, 36, 44);
+            titleFont = new Font("Arial", Font.PLAIN, 28);
+
+            font = new Font("Arial", Font.PLAIN, 12);
+
+        }
+        catch (Exception e){
+            e.printStackTrace();
+        }
+
     }
 
     public void init(){}
-    public void update(){}
-    public void draw(Graphics2D g){}
-    public void keyPressed(int k){}
+    public void update(){
+        bg.update();
+    }
+    public void draw(Graphics2D g){
+        //Title
+        bg.draw(g);
+
+        //Title
+        g.setColor(titleColor);
+        g.setFont(titleFont);
+        g.drawString("Platformer", 80, 70);
+
+        //Menu
+        g.setFont(font);
+
+        for (int i = 0; i < options.length; i++){
+
+            if (i == currentChoice){
+                g.setColor(Color.BLACK);
+
+            }
+            else{
+                g.setColor(Color.RED);
+            }
+
+            g.drawString(options [i], 145, 140 + i * 15);
+        }
+    }
+
+    private void select(){
+        if (currentChoice == 0){
+            //TODO Start
+
+        }
+        if (currentChoice == 1){
+            //TODO Help
+
+        }
+        if (currentChoice == 2){
+            System.exit(0);
+
+        }
+
+
+    }
+
+    public void keyPressed(int k){
+        if (k == KeyEvent.VK_ENTER){
+            select();
+        }
+
+        if (k == KeyEvent.VK_UP){
+            currentChoice--;
+
+            if (currentChoice == -1){
+                currentChoice = options.length - 1;
+            }
+
+        }
+
+        if (k == KeyEvent.VK_DOWN){
+            currentChoice++;
+
+            if (currentChoice == options.length){
+                currentChoice = 0;
+            }
+        }
+
+    }
     public void keyReleased(int k){}
 
 
